3ede34c9e7929ee3ce7670b01ae4021a76a6d4e3
Bastian Vobig <154318378+bvobig@users.noreply.github.com>
2026-08-26 09:16:14 +0200

    - adapted some minor layout topics

--- camii_analysis.m
+++ camii_analysis.m
@@ -71,7 +71,7 @@ results.typestotal=typestotal;
 function [data, segments, feats, stats, types, typestotal] = analysis (mttbdata, model, options) %typestruct
 
     data = preproc(mttbdata, 1); disp("1/7 Preprocessing finished")% preprocess mttb data
-    segments = segment_data(data, 1); disp("2/7 segmentation finished") % segment data (data, bend_threshold)
+    segments = segment_data(data, 1); disp("2/7 Segmentation finished") % segment data (data, bend_threshold)
     feats = featcalc(data, segments, options.zero_threshold); disp("3/7 Feature Calculation finished") % calculate features (data, segments, zerothresh)
     [obsc, obst] = feat2obs (feats); disp("4/7 Observation Transformation finished") % convert features into observations
     [predsc, predst] = applymodel (model, obsc, obst); disp("5/7 Interaction Type Prediction finished") % apply model
